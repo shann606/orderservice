6ead3dac8cf6e35b075dacffe41e1f06b6054e19
shann606 <shann606@gmail.com>
2025-09-26 09:44:10 +0530

    added other files

---
+++ src/main/java/com/ecom/orderservice/controller/OrderController.java
@@ -0,0 +1,8 @@
+package com.ecom.orderservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+}
---
+++ src/main/java/com/ecom/orderservice/dto/ExceptionDTO.java
@@ -0,0 +1,17 @@
+package com.ecom.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExceptionDTO {
+	
+	private String status;
+	private String details;
+
+}
